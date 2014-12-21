cebfc501759c7549d197b12d1421d80ca0bf7b23
王佳怡 <wangjiayi.p@gmail.com>
2014-12-21 22:46:51 +0800

    menu

--- include/init.inc.php
+++ include/init.inc.php
@@ -170,7 +170,9 @@ if( OSAdmin::checkNoNeedLogin($action_url,$no_need_login_page) ){
 
 					
 
+$user_info = UserSession::getSessionInfo();
 
+Template::assign('user_province_id',4);
 Template::assign ( 'osa_templates', $OSA_TEMPLATES);
 $sidebarStatus=$_COOKIE['sidebarStatus']==null?"yes":$_COOKIE['sidebarStatus'];
 Template::assign ( 'sidebarStatus', $sidebarStatus);
