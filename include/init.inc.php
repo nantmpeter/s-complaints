fd316a57d528779018f1916c34b818a3d2fc1c7e
王佳怡 <wangjiayi.p@gmail.com>
2014-12-21 22:52:36 +0800

    change to normal

--- include/init.inc.php
+++ include/init.inc.php
@@ -172,7 +172,7 @@ if( OSAdmin::checkNoNeedLogin($action_url,$no_need_login_page) ){
 
 $user_info = UserSession::getSessionInfo();
 
-Template::assign('user_province_id',4);
+Template::assign('user_province_id',isset($user_info['province_id'])?$user_info['province_id']:0);
 Template::assign ( 'osa_templates', $OSA_TEMPLATES);
 $sidebarStatus=$_COOKIE['sidebarStatus']==null?"yes":$_COOKIE['sidebarStatus'];
 Template::assign ( 'sidebarStatus', $sidebarStatus);
